b016fc94c14d1633ec51c96952a74475e0da3cb6
Jean-Baptiste Kempf <jb@videolan.org>
2011-02-20 19:52:26 +0100

    Qt: fix the postproc tooltip in SPrefs

--- modules/gui/qt4/components/simple_preferences.cpp
+++ modules/gui/qt4/components/simple_preferences.cpp
@@ -433,7 +433,7 @@ SPrefsPanel::SPrefsPanel( intf_thread_t *_p_intf, QWidget *_parent,
             CONFIG_GENERIC_NO_BOOL( "server-port", Integer, ui.portLabel,
                                     UDPPort );
             CONFIG_GENERIC( "http-proxy", String , ui.httpProxyLabel, proxy );
-            CONFIG_GENERIC_NO_BOOL( "ffmpeg-pp-q", Integer, ui.ppLabel,
+            CONFIG_GENERIC_NO_BOOL( "postproc-q", Integer, ui.ppLabel,
                                     PostProcLevel );
             CONFIG_GENERIC( "avi-index", IntegerList, ui.aviLabel, AviRepair );
 
