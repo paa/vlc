5ce30b7c0abab7b53d93a9f3a6a8c57d16781bcb
Felix Paul Kühne <fkuehne@videolan.org>
2011-04-30 22:32:43 +0200

    macosx: fixed Reveal-in-Finder when path contains spaces or umlauts
    closes #4721

--- modules/gui/macosx/playlist.m
+++ modules/gui/macosx/playlist.m
@@ -706,7 +706,7 @@
                     inNode: (playlist_item_t *)p_node
                     checkItemExistence:(BOOL)b_check
 {
-    [self isItem:p_item inNode:p_node checkItemExistence:b_check locked:NO];
+    return [self isItem:p_item inNode:p_node checkItemExistence:b_check locked:NO];
 }
 
 /* This method is useful for instance to remove the selected children of an
@@ -891,7 +891,7 @@
     if(! p_item || !p_item->p_input )
         return;
     
-    char *psz_uri = input_item_GetURI( p_item->p_input );
+    char *psz_uri = decode_URI( input_item_GetURI( p_item->p_input ) );
     if( psz_uri )
         o_mrl = [NSMutableString stringWithUTF8String: psz_uri];
 
@@ -1586,11 +1586,8 @@
 
         /* Refuse to move items that are not in the General Node
            (Service Discovery) */
-        if( ![self isItem: [o_item pointerValue] inNode:
-                        p_playlist->p_local_category checkItemExistence: NO] &&
-            var_CreateGetBool( p_playlist, "media-library" ) &&
-            ![self isItem: [o_item pointerValue] inNode:
-                        p_playlist->p_ml_category checkItemExistence: NO] ||
+        if( (![self isItem: [o_item pointerValue] inNode: p_playlist->p_local_category checkItemExistence: NO] &&
+            var_CreateGetBool( p_playlist, "media-library" ) && ![self isItem: [o_item pointerValue] inNode: p_playlist->p_ml_category checkItemExistence: NO]) ||
             [o_item pointerValue] == p_playlist->p_local_category ||
             [o_item pointerValue] == p_playlist->p_ml_category )
         {
