3239bdcb25edcf9ccf481bd938816352842e9087
Rémi Denis-Courmont <remi@remlab.net>
2011-05-14 13:26:52 +0300

    Disable PA by default. Too buggy.

--- modules/audio_output/pulse.c
+++ modules/audio_output/pulse.c
@@ -103,7 +103,7 @@ static void uninit(aout_instance_t *p_aout);
 vlc_module_begin ()
     set_shortname( "PulseAudio" )
     set_description( N_("Pulseaudio audio output") )
-    set_capability( "audio output", 160 )
+    set_capability( "audio output", 0 )
     set_category( CAT_AUDIO )
     set_subcategory( SUBCAT_AUDIO_AOUT )
     add_shortcut( "pulseaudio" )
