0d2c6ce99cbe81d80f0e4da6ff9024a4d52f3e4c
Rémi Denis-Courmont <remi@remlab.net>
2011-07-19 18:19:08 +0300

    GnuTLS: update version check (consistent with configure.ac)

--- modules/misc/gnutls.c
+++ modules/misc/gnutls.c
@@ -130,7 +130,7 @@ static int gnutls_Init (vlc_object_t *p_this)
         goto error;
     }
 
-    const char *psz_version = gnutls_check_version ("1.3.3");
+    const char *psz_version = gnutls_check_version ("1.7.4");
     if (psz_version == NULL)
     {
         msg_Err (p_this, "unsupported GnuTLS version");
