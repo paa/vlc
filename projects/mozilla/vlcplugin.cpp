c456a74975543c2d207d76f77c5531431d98250f
Sergey Radionov <RSATom@gmail.com>
2011-07-29 19:36:04 +0700

    NPAPI: Win32, fixed crash on plugin unload.
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

--- projects/mozilla/vlcplugin.cpp
+++ projects/mozilla/vlcplugin.cpp
@@ -522,6 +522,10 @@ NPError VlcPlugin::init(int argc, char* const argn[], char* const argv[])
 
 VlcPlugin::~VlcPlugin()
 {
+#ifdef XP_WIN
+    _WindowsManager.DestroyWindows();
+#endif
+
     free(psz_baseURL);
     free(psz_target);
     free(psz_text);
